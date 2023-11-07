e08c99e1c1e29ce062939b6bd46ebcc0a48ac62c
thegenzdev <thegenzdev@gmail.com>
2023-11-07 20:12:25 +0300

    testing

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,48 +5,49 @@
     delimited by spaces. Javascript should count as javascript, but java should
     not).
 """
-import re
 import requests
-headers = {'user-agent': 'ubuntu:hbtn:v1.0\
- (by /u/Brandixitor)'}
 
+def count_words(subreddit, word_list, after=None, word_count={}):
+    if not word_list:
+        return
 
-def count_words(subreddit, word_list, after='', occurs={}):
-    """
-        Recursive function that queries the Reddit API, parses the title of all
-        hot articles, and prints a sorted count of given keywords
-        (case-insensitive, delimited by spaces.
-        Javascript should count as javascript, but java should not).
-    """
-    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
-    response = requests.get(url, headers=headers)
-    try:
+    headers = {'User-Agent': 'Reddit Keyword Counter'}
+    base_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 100}
+    if after:
+        params['after'] = after
+
+    response = requests.get(base_url, headers=headers, params=params)
+
+    if response.status_code == 200:
         data = response.json()
-    except:
-        return
-    if (str(response.status_code) == '404'):
-        return
-    dataLength = len(data['data']['children'])
-    if (dataLength is 0):
-        return
-    for i in range(0, dataLength):
-        try:
-            get_title = data['data']['children'][i]['data']['title']
-            for a in word_list:
-                try:
-                    occurs[a]
-                except KeyError:
-                    occurs[a] = 0
-                finally:
-                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
-                                         '', get_title)[1]
-        except:
-            pass
-    afterVal = data['data']['after']
-    if (afterVal is not None):
-        return count_words(subreddit, word_list, afterVal, occurs)
+        posts = data['data']['children']
+
+        for post in posts:
+            title = post['data']['title']
+            for word in word_list:
+                if title.lower().count(word.lower()):
+                    if word in word_count:
+                        word_count[word] += title.lower().count(word.lower())
+                    else:
+                        word_count[word] = title.lower().count(word.lower())
+
+        new_after = data['data']['after']
+        if new_after:
+            count_words(subreddit, word_list, new_after, word_count)
+        else:
+            sorted_word_count = sorted(word_count.items(), key=lambda item: (-item[1], item[0]))
+            for word, count in sorted_word_count:
+                print(f'{word}: {count}')
     else:
-        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
-            if (occurs[key] > 0):
-                print("{}: {}".format(key, occurs[key]))
-        return
+        print("Subreddit not found or an error occurred.")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        subreddit = sys.argv[1]
+        keywords = [x.lower() for x in sys.argv[2].split()]
+        count_words(subreddit, keywords)
